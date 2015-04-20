a5a81d3dc599686d30c26f2222e2676343d97fb4
Maciej Stanik <M.Stanik@kainos.com>
2015-04-20 23:18:40 +0200

    Initial Commit

---
+++ compass/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  compass
+//
+//  Created by Maciej Stanik on 20/04/2015.
+//  Copyright (c) 2015 Maciej Stanik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
